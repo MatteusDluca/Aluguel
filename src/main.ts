58e11d92873f2e1825bec15837a76a6d806df5f3
Matteus <matteusdlucas5@gmail.com>
2024-09-25 01:16:41 -0300

    Authorization

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
     methods: 'GET,POST,DELETE,PUT,PATCH,HEAD',
-    allowedHeaders: ['Content-type'],
+    allowedHeaders: ['Content-type', 'Authorization'],
   })
   
   const options = new DocumentBuilder()
